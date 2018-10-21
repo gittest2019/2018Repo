c4c8af171d6c67e2702c050a3bc5204aca7867a8
pavani <pavani@pavanis-mbp.lan>
2018-10-20 19:50:27 -0500

    test

--- gitbuilddemo/src/com/test/git/GitTest2.java
+++ gitbuilddemo/src/com/test/git/GitTest2.java
@@ -3,7 +3,7 @@ package com.test.git;
 public class GitTest2 {
 
 	public static void main(String[] args) {
-		 System.out.println("Print Second GIT message..");
+		 System.out.println("Print Second GIT message..HIIIIII");
 
 	}
 
