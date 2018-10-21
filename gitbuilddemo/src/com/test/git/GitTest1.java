803b99a6e3f90e6ffdf9d27311fe01819f1009b4
pavani <pavani@pavanis-mbp.lan>
2018-10-20 19:42:11 -0500

    second Commit and Push

--- gitbuilddemo/src/com/test/git/GitTest1.java
+++ gitbuilddemo/src/com/test/git/GitTest1.java
@@ -1,6 +1,12 @@
 package com.test.git;
 
 public class GitTest1 {
+	
+	public void printMessage() {
+		
+		System.out.println("printMessage");
+		
+	}
 
 	public static void main(String args[]) {
 		 System.out.println("Print GIT message..");
